ffb7007d06e1fe638a9cadf6e2b0e42383c7a647
Curtis Helsel <curtisdhelsel@knights.ucf.edu>
2018-07-18 15:59:55 -0400

    added fcfs function

--- fcfs.go
+++ fcfs.go
@@ -8,29 +8,75 @@ import (
 
 func createProcess(process [][]int){
 
+	printData(process,4)
+
 	for i:= 0; i < len(process); i++{
-		process[i][0] = rand.Intn(10 - 1) 
-		process[i][1] = rand.Intn(10 - 1) + 1
-		//printStatus((i+1) % 4)
+		process[i][0] = rand.Intn(9) + 1
+		process[i][1] = rand.Intn(9) + 1
+
+		for j := 0; j < 3; j++ {
+			printData(process, (j) % 3)
+		}
 	}
+	
+	printData(process,3)
 	
 }
 
-func printStatus(code int){
+func getProcessList(process [][]int){
 	
-	switch code{
-	case 1:
+
+	fmt.Println("Process\tArrival\tBurst")
+	for i := 0; i < len(process); i++{
+		
+		if process[i][0] == 0 {
+			fmt.Printf("P%d: Not created\n", i+1)
+		} else{
+
+			fmt.Printf("P%d:\t%4d\t%3d\n", i+1, process[i][0], process[i][1])	
+		}
+	}
+
+	fmt.Println()
+}
+
+
+func getStatus(status int){
+	
+	fmt.Printf("Status: ")
+
+	switch status{
+	case 0:
 		fmt.Println("Generating processes.")
-		time.Sleep(time.Second)
-	case 2:
+	case 1:
 		fmt.Println("Generating processes..")
-		time.Sleep(time.Second)
-	case 3:
+	case 2:
 		fmt.Println("Generating processes...")
-		time.Sleep(time.Second)
+	case 3:
+		fmt.Println("Processes generated!")
+	case 4:
+		fmt.Println("Starting Algorthm")
 	default:
 	}
 
+	fmt.Println()
+
+}
+
+func getQueue(queue []int){
+	
+}
+
+func printData(process [][]int, status int){
+
+	for i := 0; i < 35; i++{
+		fmt.Printf("-")
+	}
+	fmt.Println()
+	getStatus(status)
+	getProcessList(process)
+	getQueue(queue)
+	time.Sleep(time.Second)
 }
 
 func fcfs(process [][]int){
@@ -39,11 +85,13 @@ func fcfs(process [][]int){
 	var cur int = -1
 
 	fmt.Println(process)
+	limit := len(process)
 
 	for i := 0; i < 100; i++{
 		for j := 0; j < len(process); j++{
 			
 			if process[j][0] == i {
+				fmt.Printf("Time %d : %d arrived\n", i, j)
 				queue = append(queue, j)
 			}
 		}
@@ -55,6 +103,7 @@ func fcfs(process [][]int){
 
 			if process[cur][1] == 0 { 
 				cur = -1	
+				limit--
 			}
 		}
 
@@ -62,11 +111,19 @@ func fcfs(process [][]int){
 			cur = queue[0]
 			queue = queue[1:]
 		}
+		
+		if limit == 0{
+			fmt.Println(i)
+			break
+		}
+
 	
 		
 	}
 	
 }
+
+
 func main() {
 
 	process := make([][]int, 5)
@@ -75,12 +132,13 @@ func main() {
 	}	
 	
 	rand.Seed(time.Now().Unix())
+	
 
 //	printStatus(1)
 
 	createProcess(process)
 	
-	fcfs(process)
+//	fcfs(process)
 
 
 }
